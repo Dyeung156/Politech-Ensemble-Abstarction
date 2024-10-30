970d44322f5405d49fd097da8d83dbf72cd27ff6
David Yeung <dyeung156@gmail.com>
2024-10-30 14:00:54 -0400

    Had to do some stuff to move git file

--- Work/testing.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import json
-
-with open("Georgia-1000.json", "r") as json_file:
-    json_data = json.load(json_file)
-    
-#plans -> map -> "districts" -> specific district -> values
-print(json_data["plans"][0]["districts"][0]["HVAP"])
--- testing.py
+++ testing.py
@@ -1,8 +1,34 @@
 import pandas as pd
 import json
 
+#open up JSON file
 with open("Georgia-1000.json", "r") as json_file:
     json_data = json.load(json_file)
-    
+#skip annoying index
+json_data = json_data["plans"]
+
+def get_map(map : int):
+    return json_data[map]
+
+def get_HVAP(district):
+    return district["HVAP"]
+
+def get_WVAP(district):
+    return district["WVAP"]
+
+def get_BVAP(district):
+    return district["BVAP"]
+
+def get_AminVAP(district):
+    return district["AMINVAP"]
+
+def get_AsianVAP(district):
+    return district["ASIANVAP"]
+
+def get_NHPIVAP(district):
+    return district["NHPIVAP"]
+
 #plans -> map -> "districts" -> specific district -> values
-print(json_data["plans"][0]["districts"][0]["HVAP"])
+print(json_data[0]["districts"][0]["HVAP"])
+print(json_data[1]["districts"][0]["HVAP"], "\n")
+print(f"Function returned: {get_HVAP(json_data[1]["districts"][0])}")
