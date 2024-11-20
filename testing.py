f353089af0ec45b20ce768ca434c9f787d678081
David Yeung <dyeung156@gmail.com>
2024-11-20 00:30:13 -0500

    changed function to return a list with the map index and a tuple to describe the map. Added create_map_tuple() as support function. Changed main method a bit too to reflect changes

--- testing.py
+++ testing.py
@@ -6,6 +6,8 @@ import os
 load_dotenv()
 THRESHOLD = float(os.getenv("THRESHOLD"))
 
+minorities = ["RacialDemographic.HISPA","RacialDemographic.BLACK","RacialDemographic.ASIAN"]
+
 #dictionary to hold map indices 
 map_indices = dict()
 
@@ -21,17 +23,34 @@ def get_num_districts(file_path):
         if df.iloc[row_index]["District"] == 1:
             #the dataframe is 0-indexed so the current row index would suffice
             return row_index
-        
-#Description: returns a set of maps for each minority group that contains an opportunity district for that minority group
+
+#Description: returns a tuple that describes the map through opportunity districts. The tuple format is:
+#             (hispanic districts, black districts, asian districts)
+#Parameters: df (Object) - dataframe containing the ensemble
+#            num_districts (int) - the number of districts in a map
+#            row (int) - the first row in df for the map 
+#returns: the map tuple that describe the map
+def create_map_tuple(df , num_districts : int, row : int):
+    district_counts = [0, 0 ,0]
+    total_population = df.iloc[row]["RacialDemographic.TOTAL"]
+    
+    #look at the districts in the map
+    for index in range(num_districts):
+        #see if it's an opportunity district for any group 
+        length = len(minorities)
+        for group_index in range(0, length):
+            minority_population = df.iloc[row + index][minorities[group_index]] 
+            if (minority_population/total_population) >= THRESHOLD: 
+                district_counts[group_index] += 1
+    
+    return tuple(district_counts)
+
+#Description: returns a list of each map and a tuple to describe it
 #parameters: file_path (str) - path to the csv file 
-#returns: dict - keys (minority groups), value (maps with opporuntiy districts with that minority group)
+#returns: list(tuple) - each tuple is (map number, map tuple)
 def opportunity_district_maps(file_path):
     df = pd.read_csv(file_path) 
-    indexes = dict() 
-    minorities = ["RacialDemographic.HISPA","RacialDemographic.BLACK","RacialDemographic.ASIAN"]
-    # intiailize the dictionary 
-    for m in minorities:
-        indexes[m] = set() 
+    maps_info = list()
 
     row = 0
     cur_map = 0 
@@ -39,27 +58,22 @@ def opportunity_district_maps(file_path):
     
     #go thru each row in the dataframe
     while row < len(df): 
-        #look at every district in the map 
-        for _ in range(num_districts):
-            total_population = df.iloc[row]["RacialDemographic.TOTAL"]
-            
-            #check for each ethnic group for possible opportunity district
-            for m in minorities: 
-                minority_population = df.iloc[row][m] 
-                if (minority_population/total_population) >= THRESHOLD: 
-                    indexes[m].add(cur_map) 
-                    
-            row+=1
+        #create the map tuple for each map and add it to the list
+        maps_info.append( (cur_map, create_map_tuple(df, num_districts, row)) )
+        
+        #move row to the next map
+        row += num_districts
         cur_map+=1
-    return indexes
+        
+    return maps_info
     
 if __name__ == "__main__":
     file_path = "output.csv"
-    opportunity_districts = opportunity_district_maps(file_path)
+    opportunity_maps = opportunity_district_maps(file_path)
 
-    minorities = ["RacialDemographic.HISPA","RacialDemographic.BLACK","RacialDemographic.ASIAN"]
-
-    for m in minorities:
-        print(m, len(opportunity_districts[m]))
+    for map in opportunity_maps:
+        print(map)
+    # for m in minorities:
+    #     print(m, len(opportunity_districts[m]))
         
     
