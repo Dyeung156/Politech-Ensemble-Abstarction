f3956338011116ed60d9dc0b37c4355e0f128b38
David Yeung <dyeung156@gmail.com>
2025-04-06 01:25:18 -0400

    deleted old react files before the move to typescript and react redux

--- my-react-app/src/util.jsx
+++
@@ -1,8 +0,0 @@
-// description: turns a string into an array [int]
-// parameters: str_tuple (str) - the string to convert to an array
-// returns: an array with only integers in it 
-export function strToArr(str_tuple){
-    return str_tuple
-    .match(/\d+/g)
-    .map(num => parseFloat(num));
-}
