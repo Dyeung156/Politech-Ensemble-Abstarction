d020d322d75ef95c33262bbc9101fc802e53c353
David Yeung <dyeung156@gmail.com>
2025-04-15 17:29:03 -0400

    Managed to get some dots on the circle.

--- cluster-visulization-techniques/src/components/CircleVisual.tsx
+++ cluster-visulization-techniques/src/components/CircleVisual.tsx
@@ -3,14 +3,39 @@ import type point from "@/util/pointType";
 import cluster_measures from "@/assets/cluster_measures.json";
 import opp_district_data from "@/assets/opportunity_districts.json";
 
-function pointPlacement(measures_data: [string, unknown][], cluster_name: string, cluster_value: number)
+interface ClusterMeasures {
+    cluster: [string, number[]][]
+};
+
+function pointPlacement(measuresData: [string, number[]][], clusterValue: string, radius: number) 
 {
+  let avgRadiusPercent = 0;
+  let avgAngle = 0;
+  for (const measure of measuresData) 
+  {
+    if (measure[0] != clusterValue) 
+      continue;
+
+    avgRadiusPercent = (measure[1][0] * radius) / 100;
+    avgAngle = measure[1][1] * Math.PI / 180;
+
+  }
+
+  console.log(avgRadiusPercent)
+  const x = Math.cos(avgAngle) * (avgRadiusPercent * radius);
+  const y = Math.sin(avgAngle) * (avgRadiusPercent * radius);
+
+  return { x: x + 150, y: y + 150 };
 }
 
 
-export default function CircleVisual() {
+export default function CircleVisual() 
+{
   const opp_district_clusters = Object.entries(opp_district_data);
-  const measures = Object.entries(cluster_measures);
+  const measureTesting: [string, number[]][] = Object.entries(cluster_measures["Opportunity Districts"]);
+  console.log(measureTesting);
+  // console.log(measures);
+  // console.log(pointPlacement(measures, "4", 125));
 
   return (
     <div>
@@ -20,8 +45,9 @@ export default function CircleVisual() {
 
             {
               Array.from(opp_district_clusters).map((clusterPair, index) => {
+                // const measureData: ClusterMeasures = cluster_measures["Opportunity Districts"][clusterPair[0]];
                 return <ClusterButton key={index}
-                  point={pointPlacement(clusterPair)}
+                  point={pointPlacement(measureTesting, clusterPair[0], 125)}
                   mapData={clusterPair}
                   className="tooltip bg-amber-100" />
               })
